9f7078181132f244fb526b8155cda4ff823cecb2
Rajeevkr <rajeevrajeng@gmail.com>
2026-04-11 15:50:07 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import HomePage from './Pages/HomePage'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+        <Route path='/' element={<HomePage/>} />
+
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </>
+    
   )
 }
